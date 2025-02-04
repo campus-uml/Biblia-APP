a2b3c289b403c7e367cbfa4a11762a1f4ae98d86
EDITHELIZABETH <est.ehernandez|0301@uml.edu.ni>
2025-02-03 18:47:04 -0600

    ...

--- public/next.config.js
+++ public/next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    env: {
-      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-      NEXT_PUBLIC_BIBLE_API_KEY: process.env.NEXT_PUBLIC_BIBLE_API_KEY,
-    },
-  }
-  
-  module.exports = nextConfig
-  
-  
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
+
