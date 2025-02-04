712e09ab6e36f7fdb44f45baf756427c72ae704a
EDITHELIZABETH <est.ehernandez|0301@uml.edu.ni>
2025-02-04 13:02:44 -0600

    revision

--- public/next.config.js
+++ public/next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ["lgybzwgwlhhkxggbryyr.supabase.co"],
-  },
-  experimental: {
-    appDir: true,
-  },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    NEXT_PUBLIC_BIBLE_API_KEY: process.env.NEXT_PUBLIC_BIBLE_API_KEY,
-  },
-}
+  staticPageGenerationTimeout: 120, // Tiempo de espera en segundos
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
